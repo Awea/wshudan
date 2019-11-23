1ed4dc17449acd1cd515b0729d81bd3fb371378a
climu <climmu@gmail.com>
2019-11-23 14:50:26 +0100

    fix #1 board setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
 import { h, Component } from 'preact';
 import {Goban} from '@sabaki/shudan';
+const Board = require('@sabaki/go-board');
+
 
 import style from '@sabaki/shudan/css/goban.css';
 
@@ -25,12 +27,14 @@ const signMap = [
     [ 0, 0, 0, 1,-1,-1,-1,-1,-1, 0,-1,-1, 1, 1, 0, 1, 1, 1, 0]
 ]
 
+const board = new Board(signMap)
+
 export default class App extends Component {
 	render() {
-		return h('div', {}, 
+		return h('div', {},
 			h(Goban, {
 				vertexSize: 24,
-				signMap
+				signMap: board.signMap
 			})
 		);
 	}
