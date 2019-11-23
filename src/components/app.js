c40b0c0ef95fa3532245f9519d671cde2923e823
climu <climmu@gmail.com>
2019-11-23 21:42:41 +0100

    navigation buttons and move counter

--- src/components/app.js
+++ src/components/app.js
@@ -7,11 +7,11 @@ const GameTree = require('@sabaki/immutable-gametree')
 import style from '@sabaki/shudan/css/goban.css';
 
 // According to the tests found in https://github.com/SabakiHQ/sgf/blob/master/tests/parse.test.js the following should works but it produce a JS error - @awea 20191123
-//let rootNodes = sgf.parse('(;B[aa]SZ[19];AB[cc][dd:ee])')
-//let gameTrees = rootNodes.map(rootNode => {
-//    return new GameTree({root: rootNode})
-//})
-
+let rootNodes = sgf.parse('(;B[aa]SZ[19];B[dp];W[pq];B[dd];W[qc];B[qo];B[ch];W[cq];B[cp];W[dq];B[eq];W[er])')
+let gameTrees = rootNodes.map(rootNode => {
+    return new GameTree({root: rootNode})
+})
+var tree = gameTrees[0]
 
 const signMap = [
     [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,6 +44,7 @@ export default class App extends Component {
         this.state = {
             board: new Board(signMap),
             vertexSize: 24,
+            move : 0,
         }
     }
 
@@ -73,6 +74,57 @@ export default class App extends Component {
           this.setState(s => ({vertexSize: Math.max(s.vertexSize + 4, 4)}))
       }
     }, '+'),
+    h('button', {
+      type: 'button',
+      onClick: evt => {
+        var node = tree.get(this.state.move)
+        if (node == null){
+          return
+        }
+        if (node.data.B != null) {
+          var vertex = sgf.parseVertex(node.data.B[0])
+          var sign = 1
+        }
+        else if (node.data.W != null) {
+          var vertex = sgf.parseVertex(node.data.W[0])
+          var sign = -1
+        }
+        else{
+          return
+        }
+        let newBoard = this.state.board.set(vertex, 0)
+        this.setState({board: newBoard})
+        this.setState({move: this.state.move - 1})
+
+      }
+    }, '<'),
+    h('button', {
+      type: 'button',
+      onClick: evt => {
+        var newmove = this.state.move + 1
+        var node = tree.get(newmove)
+        if (node == null){
+          return
+        }
+        if (node.data.B != null) {
+          var vertex = sgf.parseVertex(node.data.B[0])
+          var sign = 1
+        }
+        else if (node.data.W != null) {
+          var vertex = sgf.parseVertex(node.data.W[0])
+          var sign = -1
+        }
+        else{
+          return
+        }
+        let newBoard = this.state.board.makeMove(sign, vertex)
+        this.setState({board: newBoard})
+        this.setState({move: newmove})
+
+      }
+    }, '>'),
+    h('span',{},this.state.move)
+
 
 		);
 	}
